79a520db12d4e0b1aa8ccd9ecb3789be5a0d17e8
Thiago <thiago___melo@hotmail.com>
2023-12-06 21:23:04 -0300

    first commit

---
+++ src/main/java/br/com/volmed/volmed/VolmedApplication.java
@@ -0,0 +1,13 @@
+package br.com.volmed.volmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolmedApplication.class, args);
+	}
+
+}
