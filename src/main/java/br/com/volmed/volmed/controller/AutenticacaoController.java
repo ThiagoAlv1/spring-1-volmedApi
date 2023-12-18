debd06d21099981d5a308b93b7c32607e5fdda1b
Thiago <thiago___melo@hotmail.com>
2023-12-18 18:53:19 -0300

    adicionando token JWT

--- src/main/java/br/com/volmed/volmed/controller/AutenticacaoController.java
+++ src/main/java/br/com/volmed/volmed/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package br.com.volmed.volmed.controller;
 
 import br.com.volmed.volmed.domain.usuario.DadosAutenticacao;
+import br.com.volmed.volmed.domain.usuario.Usuario;
+import br.com.volmed.volmed.infra.security.DadosTokenJWT;
+import br.com.volmed.volmed.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AutenticacaoController {
 
+    //O AuthenticationManager no Spring é responsável por autenticar usuários durante o processo de login.
     @Autowired
     private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    //Retornando token jwt
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
---
+++ src/main/java/br/com/volmed/volmed/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package br.com.volmed.volmed.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
