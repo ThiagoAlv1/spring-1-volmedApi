9a39bbdf8d91636942c07da4541be6faa0179e8c
Thiago <thiago___melo@hotmail.com>
2023-12-21 22:34:54 -0300

    curso 2 concluido

--- src/main/java/br/com/volmed/volmed/infra/security/TokenService.java
+++ src/main/java/br/com/volmed/volmed/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import br.com.volmed.volmed.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class TokenService {
     @Value("${api.security.token.secret}")
     private String secret;
 
+    //Criando token JWT
     public String gerarToken(Usuario usuario) {
         try {
             var algoritmo = Algorithm.HMAC256(secret);
@@ -29,6 +31,21 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println("TOKEN: " +tokenJWT);
+            exception.printStackTrace();
+            throw new RuntimeException("Token JWT inválido ou expirado");
         }
     }
 
